c9c739762d340a30d97c261f2137424d45b62cc7
Utsav Jana <utsav.j@1channel.co>
2025-06-22 11:17:31 +0530

    initial contract setup done

--- contracts/src/ConstantProductAMM.sol
+++ contracts/src/ConstantProductAMM.sol
@@ -4,9 +4,14 @@ pragma solidity ^0.8.20;
 import "contracts/lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 import "contracts/lib/openzeppelin-contracts/contracts/utils/ReentrancyGuard.sol";
 
-
-
 contract ConstantProductAutomatedMarketMaker is ReentrancyGuard{
+
+    //events
+    event LiquidityAdded(address indexed provider, uint256 shares);
+    event LiquidityRemoved(address indexed provider, uint256 shares);
+    event Swapped(address indexed user, address tokenIn, uint256 amountIn, address tokenOut, uint256 amountOut);
+    event ReservesUpdated(uint256 reserve0, uint256 reserve1);
+
     IERC20 private immutable token0;
     IERC20 private immutable token1;
 
@@ -33,27 +38,36 @@ contract ConstantProductAutomatedMarketMaker is ReentrancyGuard{
         }
         else{
             require(reserve0 * _reserveAdded1 == reserve1 * _reserveAdded0, "Invalid ratio"); //maintain ratio
-            shares= _min((_reserveAdded0/reserve0)* totalSupply, (_reserveAdded1/reserve1)*totalSupply);
+            shares= _min((_reserveAdded0*totalSupply) / reserve0, (_reserveAdded1*totalSupply) / reserve1);
         }
 
         require(shares > 0, "AMM: Invalid shares");
 
         _mintShares(msg.sender, shares);
         _updateReserves();
+        emit LiquidityAdded(msg.sender, shares);
+        return shares;
     }
 
     function removeLiquidity(uint256 _shares) external nonReentrant returns (uint256 reserveRemoved0, uint256 reserveRemoved1){
         require(_shares > 0, "AMM: Invalid shares");
         require(lpBalance[msg.sender] >= _shares, "AMM: Insufficient shares");
 
-        reserveRemoved0= (_shares* reserve0)/totalSupply;
-        reserveRemoved1= (_shares* reserve1)/totalSupply;
+        uint256 balance0 = token0.balanceOf(address(this));
+        uint256 balance1 = token1.balanceOf(address(this));
+        reserveRemoved0= (_shares * balance0)/totalSupply;
+        reserveRemoved1= (_shares * balance1)/totalSupply;
 
         require(reserveRemoved0 > 0 && reserveRemoved1 > 0, "AMM: Invlaid reserves");
-
         _burnShares(msg.sender, _shares);
 
+        token0.transfer(msg.sender, reserveRemoved0);
+        token1.transfer(msg.sender, reserveRemoved1);
+
         _updateReserves();
+        emit LiquidityRemoved(msg.sender, _shares);
+
+        return (reserveRemoved0, reserveRemoved1);
     }
 
     function swap(address _tokenIn, uint256 _amountIn) external nonReentrant returns(uint256 amountOut){
@@ -73,7 +87,9 @@ contract ConstantProductAutomatedMarketMaker is ReentrancyGuard{
         tokenOut.transfer(msg.sender, amountOut);
 
         _updateReserves();
+        emit Swapped(msg.sender, _tokenIn, _amountIn, isToken0 ? address(token1) : address(token0), amountOut);
 
+        return amountOut;
     }
 
     function _sqrt(uint256 x) internal pure returns(uint256 z){
@@ -108,5 +124,21 @@ contract ConstantProductAutomatedMarketMaker is ReentrancyGuard{
     function _updateReserves() internal {
         reserve0 = token0.balanceOf(address(this));
         reserve1= token1.balanceOf(address(this));
+        emit ReservesUpdated(reserve0, reserve1);
+    }
+
+    //getters
+
+    function getAmountOut(address _tokenIn, uint256 _amountIn) external view returns(uint256 amountOut){
+        require(_tokenIn== address(token0) || _tokenIn == address(token1), "AMM: Invalid token contract");
+        require(_amountIn > 0, "AMM: Invalid token amount");
+
+        bool isToken0 = (_tokenIn == address(token0));
+        (uint256 reserveIn, uint256 reserveOut)=
+            isToken0 ? (reserve0, reserve1) : (reserve1, reserve0);
+        
+        amountOut= (reserveOut * _amountIn) / (reserveIn + _amountIn);
+        require(amountOut > 0, "AMM: Invalid amount out");
+        return amountOut;
     }
 }
