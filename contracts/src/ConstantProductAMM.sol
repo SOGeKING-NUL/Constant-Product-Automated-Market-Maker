7d438e23ecac111385fb18e658d8f65ebbac2693
Utsav Jana <utsav.j@1channel.co>
2025-06-22 20:10:18 +0530

    mock 1 finished

--- contracts/src/ConstantProductAMM.sol
+++ contracts/src/ConstantProductAMM.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "contracts/lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
-import "contracts/lib/openzeppelin-contracts/contracts/utils/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
 contract ConstantProductAutomatedMarketMaker is ReentrancyGuard{
 
