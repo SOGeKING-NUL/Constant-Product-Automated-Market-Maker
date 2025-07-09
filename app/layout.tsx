ecf54ea5b64427591017e08a485cecd30ccbe062
Utsav Jana <utsav.j@1channel.co>
2025-07-09 09:56:26 +0530

    added metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,10 +12,37 @@ const inter = Inter({
 })
 
 export const metadata: Metadata = {
-  title: "v0 App",
-  description: "Created with v0",
-  generator: "v0.dev",
-}
+  title: "xyk-amm",
+  description: "A Constant Product Automated Market Maker for a WETH/USDC pair- provide liquidity and perform swaps",
+  icons: {
+    icon: "/images/favicon.ico",
+    shortcut: "/images/favicon.ico",
+    apple: "/images/favicon.ico",
+  },
+  openGraph: {
+    title: "xyk-amm",
+    description: "A Constant Product Automated Market Maker for a WETH/USDC pair- provide liquidity and perform swaps",
+    url: "https://xyk-amm.vercel.app/", 
+    siteName: "xyk-amm",
+    images: [
+      {
+        url: "/images/preview.jpg", 
+        width: 1200,
+        height: 630,
+        alt: "Constant Product Automated Market Maker for a WETH/USDC",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "x.y=k",
+    description: "Constant Product AMM to understan liquidity pools and perform swaps",
+    images: ["/images/preview.jpg"], 
+    site: "@JanaUtsav", 
+  },
+};
+
 
 export default function RootLayout({
   children,
